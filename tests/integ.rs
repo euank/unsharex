7d8b4235701b1a990c3427b6ced593b1a9f82733
Euan Kemp <euank@euank.com>
2023-08-05 20:17:07 +0900

    Add more integ tests

--- tests/integ.rs
+++ tests/integ.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::process::Command as StdCommand;
+
+use nix;
 use assert_cmd::Command;
 
 // compare impls compares 'unsharex' vs 'unshare'
@@ -97,4 +101,91 @@ fn test_netns() {
     link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
 "#.as_bytes(),
     );
+}
+
+#[test]
+fn test_make_each_ns() {
+    if users::get_effective_uid() != 0 {
+        eprintln!("skipping test that requires root");
+        return;
+    };
+
+    // Join each namespace in the two different forms you can
+    // the '-t$PID' format, and the '/file' format
+
+    // first, create some namespace files and keep em open with an unshare
+    let tmp_dir = tempfile::tempdir().unwrap();
+    let nses = vec![
+        // (long flag, proc name, short flag)
+        // long flag is also the filename of /proc/self/ns/$file
+        ("mount", "mnt", "m"),
+        ("uts", "uts", "u"),
+        ("ipc", "ipc", "i"),
+        ("net", "net", "n"),
+        ("pid", "pid", "p"),
+        ("user", "user", "U"),
+        ("cgroup", "cgroup", "C"),
+        ("time", "time", "T"),
+    ];
+
+    for ns in &nses {
+        std::fs::File::create(tmp_dir.path().join(ns.0)).unwrap();
+    }
+
+    let mut root_nses = HashMap::new();
+    for ns in &nses {
+        root_nses.insert(ns.0, std::fs::read_link(format!("/proc/self/ns/{}", ns.1)).unwrap().to_str().unwrap().to_string());
+    }
+
+    let parse_nses = |readlink_output: &str| {
+        let mut out = HashMap::new();
+        let lines = readlink_output.lines().collect::<Vec<_>>();
+        assert_eq!(lines.len(), nses.len());
+        for (i, ns) in nses.iter().enumerate() {
+            out.insert(ns.0, lines[i].to_string());
+        }
+        out
+    };
+
+    let readlink_cmd = format!("readlink /proc/self/ns/{{{}}}", nses.iter().map(|n| n.1).collect::<Vec<_>>().join(","));
+
+    for unsharex in vec![true, false] {
+        for ns in &nses {
+            match (unsharex, ns.0) {
+                (false, "mount") | (false, "user") => {
+                    // XXX: for some reason, unshare/unsharex work differently these and I don't
+                    // get why yet. Come back to this.
+                    continue
+                }
+                _ => {},
+            }
+            let nsf = tmp_dir.path().join(ns.0).to_str().unwrap().to_string();
+
+            let mut cmd = if unsharex {
+                Command::cargo_bin("unsharex").unwrap()
+            } else {
+                Command::new("unshare")
+            };
+
+            cmd.args(vec![&format!("--{}={}", ns.0, nsf)]);
+            if ns.0 == "pid" {
+                // special case, need to fork or pid namespaces don't work
+                cmd.arg("--fork");
+            }
+            cmd.args(vec!["--", "sh", "-c", &readlink_cmd]);
+            let out = cmd.unwrap();
+            let outns = parse_nses(&String::from_utf8(out.stdout).unwrap());
+
+            for ns2 in &nses {
+                if ns.0 == ns2.0 {
+                    assert_ne!(outns.get(ns2.0), root_nses.get(ns2.0));
+                } else {
+                    assert_eq!(outns.get(ns2.0), root_nses.get(ns2.0));
+                }
+            }
+
+            // and cleanup
+            let _ = nix::mount::umount(nsf.as_str());
+        }
+    }
 }
